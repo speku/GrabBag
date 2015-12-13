2d63eb593945cb19eae03c8b9bf809752b67fca7
Maximilian Roeck <wurzeltatzelwurm@gmail.com>
2015-12-13 21:00:55 +0100

    some gui adjustments

--- WoWAddonUpdater/WoWAddonUpdater/.Designer.cs
+++ WoWAddonUpdater/WoWAddonUpdater/.Designer.cs
@@ -29,18 +29,19 @@
         private void InitializeComponent()
         {
             this.AddonList = new System.Windows.Forms.SplitContainer();
+            this.LeftListTableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.AddonProgressBar = new System.Windows.Forms.ProgressBar();
             this.AddonDetails = new System.Windows.Forms.SplitContainer();
             this.AddonFacts = new System.Windows.Forms.SplitContainer();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.AddonPictureBox = new System.Windows.Forms.PictureBox();
+            this.AddonDescriptionText = new System.Windows.Forms.RichTextBox();
+            this.AddonListView = new System.Windows.Forms.ListView();
+            this.AddonFactsView = new System.Windows.Forms.ListView();
             ((System.ComponentModel.ISupportInitialize)(this.AddonList)).BeginInit();
             this.AddonList.Panel1.SuspendLayout();
             this.AddonList.Panel2.SuspendLayout();
             this.AddonList.SuspendLayout();
+            this.LeftListTableLayout.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.AddonDetails)).BeginInit();
             this.AddonDetails.Panel1.SuspendLayout();
             this.AddonDetails.Panel2.SuspendLayout();
@@ -49,8 +50,7 @@
             this.AddonFacts.Panel1.SuspendLayout();
             this.AddonFacts.Panel2.SuspendLayout();
             this.AddonFacts.SuspendLayout();
-            this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AddonPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // AddonList
@@ -61,14 +61,39 @@
             // 
             // AddonList.Panel1
             // 
-            this.AddonList.Panel1.Controls.Add(this.tableLayoutPanel1);
+            this.AddonList.Panel1.Controls.Add(this.LeftListTableLayout);
             // 
             // AddonList.Panel2
             // 
             this.AddonList.Panel2.Controls.Add(this.AddonDetails);
-            this.AddonList.Size = new System.Drawing.Size(671, 477);
-            this.AddonList.SplitterDistance = 223;
+            this.AddonList.Size = new System.Drawing.Size(731, 689);
+            this.AddonList.SplitterDistance = 242;
             this.AddonList.TabIndex = 0;
+            // 
+            // LeftListTableLayout
+            // 
+            this.LeftListTableLayout.AutoSize = true;
+            this.LeftListTableLayout.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.LeftListTableLayout.ColumnCount = 1;
+            this.LeftListTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.LeftListTableLayout.Controls.Add(this.AddonProgressBar, 0, 0);
+            this.LeftListTableLayout.Controls.Add(this.AddonListView, 0, 1);
+            this.LeftListTableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LeftListTableLayout.Location = new System.Drawing.Point(0, 0);
+            this.LeftListTableLayout.Name = "LeftListTableLayout";
+            this.LeftListTableLayout.RowCount = 2;
+            this.LeftListTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.LeftListTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.LeftListTableLayout.Size = new System.Drawing.Size(242, 689);
+            this.LeftListTableLayout.TabIndex = 0;
+            // 
+            // AddonProgressBar
+            // 
+            this.AddonProgressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AddonProgressBar.Location = new System.Drawing.Point(3, 3);
+            this.AddonProgressBar.Name = "AddonProgressBar";
+            this.AddonProgressBar.Size = new System.Drawing.Size(236, 14);
+            this.AddonProgressBar.TabIndex = 0;
             // 
             // AddonDetails
             // 
@@ -84,9 +109,9 @@
             // 
             // AddonDetails.Panel2
             // 
-            this.AddonDetails.Panel2.Controls.Add(this.richTextBox1);
-            this.AddonDetails.Size = new System.Drawing.Size(444, 477);
-            this.AddonDetails.SplitterDistance = 282;
+            this.AddonDetails.Panel2.Controls.Add(this.AddonDescriptionText);
+            this.AddonDetails.Size = new System.Drawing.Size(485, 689);
+            this.AddonDetails.SplitterDistance = 407;
             this.AddonDetails.TabIndex = 0;
             // 
             // AddonFacts
@@ -97,78 +122,65 @@
             // 
             // AddonFacts.Panel1
             // 
-            this.AddonFacts.Panel1.Controls.Add(this.flowLayoutPanel2);
+            this.AddonFacts.Panel1.Controls.Add(this.AddonFactsView);
             // 
             // AddonFacts.Panel2
             // 
-            this.AddonFacts.Panel2.Controls.Add(this.pictureBox1);
-            this.AddonFacts.Size = new System.Drawing.Size(444, 282);
-            this.AddonFacts.SplitterDistance = 200;
+            this.AddonFacts.Panel2.Controls.Add(this.AddonPictureBox);
+            this.AddonFacts.Size = new System.Drawing.Size(485, 407);
+            this.AddonFacts.SplitterDistance = 264;
             this.AddonFacts.TabIndex = 0;
             // 
-            // tableLayoutPanel1
+            // AddonPictureBox
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.progressBar1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 3);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.670913F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 95.32909F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(220, 471);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.AddonPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AddonPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.AddonPictureBox.Name = "AddonPictureBox";
+            this.AddonPictureBox.Size = new System.Drawing.Size(217, 407);
+            this.AddonPictureBox.TabIndex = 0;
+            this.AddonPictureBox.TabStop = false;
             // 
-            // progressBar1
+            // AddonDescriptionText
             // 
-            this.progressBar1.Location = new System.Drawing.Point(3, 3);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(214, 15);
-            this.progressBar1.TabIndex = 0;
+            this.AddonDescriptionText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AddonDescriptionText.Location = new System.Drawing.Point(0, 0);
+            this.AddonDescriptionText.Name = "AddonDescriptionText";
+            this.AddonDescriptionText.Size = new System.Drawing.Size(485, 278);
+            this.AddonDescriptionText.TabIndex = 0;
+            this.AddonDescriptionText.Text = "";
             // 
-            // flowLayoutPanel1
+            // AddonListView
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 24);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(214, 444);
-            this.flowLayoutPanel1.TabIndex = 1;
+            this.AddonListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AddonListView.Location = new System.Drawing.Point(3, 23);
+            this.AddonListView.Name = "AddonListView";
+            this.AddonListView.Size = new System.Drawing.Size(236, 1068);
+            this.AddonListView.TabIndex = 1;
+            this.AddonListView.UseCompatibleStateImageBehavior = false;
             // 
-            // flowLayoutPanel2
+            // AddonFactsView
             // 
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
-            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(194, 276);
-            this.flowLayoutPanel2.TabIndex = 0;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(3, 6);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(237, 273);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(4, 4);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(440, 187);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.AddonFactsView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AddonFactsView.Location = new System.Drawing.Point(0, 0);
+            this.AddonFactsView.Name = "AddonFactsView";
+            this.AddonFactsView.Size = new System.Drawing.Size(264, 407);
+            this.AddonFactsView.TabIndex = 0;
+            this.AddonFactsView.UseCompatibleStateImageBehavior = false;
             // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(671, 477);
+            this.ClientSize = new System.Drawing.Size(731, 689);
             this.Controls.Add(this.AddonList);
             this.Name = "MainMenu";
             this.Text = "MainMenu";
             this.AddonList.Panel1.ResumeLayout(false);
+            this.AddonList.Panel1.PerformLayout();
             this.AddonList.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.AddonList)).EndInit();
             this.AddonList.ResumeLayout(false);
+            this.LeftListTableLayout.ResumeLayout(false);
             this.AddonDetails.Panel1.ResumeLayout(false);
             this.AddonDetails.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.AddonDetails)).EndInit();
@@ -177,8 +189,7 @@
             this.AddonFacts.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.AddonFacts)).EndInit();
             this.AddonFacts.ResumeLayout(false);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AddonPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -187,13 +198,13 @@
 
         private System.Windows.Forms.SplitContainer AddonList;
         private System.Windows.Forms.SplitContainer AddonDetails;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel LeftListTableLayout;
+        private System.Windows.Forms.ProgressBar AddonProgressBar;
         private System.Windows.Forms.SplitContainer AddonFacts;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox AddonPictureBox;
+        private System.Windows.Forms.RichTextBox AddonDescriptionText;
+        private System.Windows.Forms.ListView AddonListView;
+        private System.Windows.Forms.ListView AddonFactsView;
     }
 }
 
